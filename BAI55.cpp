3eef5a801c52db5cbc2e4293bfe7db0dc5c2103a
Yuhsensei <giahuy8607@gmail.com>
2025-11-11 20:54:47 +0700

    Update BAI55.cpp

--- BAI55.cpp
+++ BAI55.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, k, A[100001];
-void input(){
-	scanf("%d",&n);
+void input(int A[], int n){
 	for(int i = 0; i < n; i++)
         scanf("%d ",&A[i]);
 }
-void solve(){
+void solve(int A[], int n, int k){
     for(int i = k; i < n - 1; i++)
         A[i] = A[i + 1];
-    n--;
 }
-void output(){
+void output(int A[], int n){
 	for(int i = 0; i < n; i++)
         printf("%d ",A[i]);
 }
 int main(){
-	input();
-    solve();
-    output();
+  int n, k, A[100001];
+  	scanf("%d",&n);
+	input(A, n);
+	scanf("%d",&k);
+    solve(A, n, k);
+    output(A, n - 1);
 	return 0;    
 }
