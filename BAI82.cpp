4b7cfede43ec97b1ac9955700957c9e403f65b0f
Yuhsensei <giahuy8607@gmail.com>
2025-11-11 20:39:17 +0700

    Update BAI82.cpp

--- BAI82.cpp
+++ BAI82.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, A[101][101];
-void output(){
+void output(int A[][1001], int n, int m){
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < m; j++)
 			printf("%d ",A[i][j]);
 		printf("\n");
 	}
 }
-void solve1(){
+void solve1(int A[][1001], int n, int m){
 	int dem = 1;
 	int h1 = 0, h2 = n - 1, c1 = 0, c2 = m - 1;
 	while(h1 <= h2 && c1 <= c2){
@@ -17,25 +16,27 @@ void solve1(){
 	    	dem++;
 		}
 		h1++;
+		if (h1 > h2) break;
 	  	for (int i = h1; i <= h2; i++){
 			A[i][c2] = dem;
 			dem++;
 		}
 		c2--;
+		if (c1 > c2) break;
 		for (int i = c2; i >= c1; i--){
 	    	A[h2][i] = dem;
 	    	dem++;
 		}
 	  	h2--;
+	  	if (h1 > h2) break;
 	  	for (int i = h2; i >= h1; i--){
 	  		A[i][c1] = dem;
 			dem++;	
 		}
 		c1++;
 	}
-	output();
 }
-void solve2(){
+void solve2(int A[][1001], int n, int m){
 	int dem = n * m;
 	int h1 = 0, h2 = n - 1, c1 = 0, c2 = m - 1;
 	while(h1 <= h2 && c1 <= c2){
@@ -44,27 +45,32 @@ void solve2(){
 	    	dem--;
 		}
 		h1++;
+		if (h1 > h2) break;
 	  	for (int i = h1; i <= h2; i++){
 			A[i][c2] = dem;
 			dem--;
 		}
 		c2--;
+		if (c1 > c2) break;
 		for (int i = c2; i >= c1; i--){
 	    	A[h2][i] = dem;
 	    	dem--;
 		}
 	  	h2--;
+	  	if (h1 > h2) break;
 	  	for (int i = h2; i >= h1; i--){
 	  		A[i][c1] = dem;
 			dem--;	
 		}
 		c1++;
 	}
-	output();
 }
 int main(){
+  int n, m, A[1001][1001];
 	scanf("%d %d",&n,&m);
-	solve1();
+	solve1(A, n, m);
+	output(A, n, m);
 	printf("\n");
-	solve2();
+	solve2(A, n, m);
+	output(A, n, m);
 }
