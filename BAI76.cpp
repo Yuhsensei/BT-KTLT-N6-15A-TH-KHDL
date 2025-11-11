03a72362dbcdb81b7f405ca1849292ef1ad2df21
Yuhsensei <giahuy8607@gmail.com>
2025-11-11 20:47:45 +0700

    Update BAI76.cpp

--- BAI76.cpp
+++ BAI76.cpp
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, A[1001][1001], ans = 0;
-void input(){
-	scanf("%d",&n);
+void input(int A[1001][1001], int n){
 	for(int i = 0; i < n; i++) 
         for(int j = 0; j < n; j++) 
             scanf("%d",&A[i][j]);
 }
-void output(){
+void output(int A[1001][1001], int n){
 	for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++) 
             printf("%d", A[i][j]);
         printf("\n");
     }
 }
-void solve(){
-	int Min = 1e9;
+void solve(int A[1001][1001], int n){
+	int Min = 1e9, ans = 0;
 	for(int i = 0; i < n; i++){
 		Min = A[i][0];
         for(int j = 0; j < n; j++)
@@ -25,12 +23,14 @@ void solve(){
     	ans += Min;
 	}
 	printf("ma tran sau khi doi\n");
-	output();
+	output(A, n);
 	printf("Tong cac phan tu nho nhat la %d", ans);
 }
 int main(){
-	input();
-	output();
-    solve();
+  int n, A[1001][1001];
+  scanf("%d",&n);
+	input(A, n);
+	output(A, n);
+  solve(A, n);
 	return 0;    
 }
