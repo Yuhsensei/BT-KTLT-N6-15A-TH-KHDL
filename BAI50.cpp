88c5ca69b16c5f1cc20f7e37584352d5b2cbb274
Yuhsensei <giahuy8607@gmail.com>
2025-11-11 20:57:40 +0700

    Update BAI50.cpp

--- BAI50.cpp
+++ BAI50.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-double x;
-void input(){
-	scanf("%lf",&x);
-}
-void solve(){
+
+double ch(double x){
 	double tmp = 1, ans = 1, n = 1;
 	while (fabs(tmp) >= 0.000001){
     tmp = tmp*x*x/(n*(n+1));
 		ans += tmp;
 		n = n + 2;
 	}
-	printf("%lf",ans);
+	return ans;
 }
 int main(){
-	input();
-  solve();
+  double x;
+	scanf("%lf",&x);
+  printf("%lf",ch(x));
 	return 0;    
 }
